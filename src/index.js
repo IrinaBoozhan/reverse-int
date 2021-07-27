6d03fa3dfd0fc6fc2d443eb1274380d019554bc3
Irina <bokhanko-irina@yandex.ru>
2021-07-27 22:24:10 +0300

    task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   let test = Math.abs(n)
+   const str = test.toString();
+   let rev = [];
+   for (let i = 0; i < str.length; i++) {
+      rev[i] = str[(str.length - i - 1)];
+   }
+   return +(rev.join(""));
 }
